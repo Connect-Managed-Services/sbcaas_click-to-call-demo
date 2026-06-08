335b25a9e195aad184959cc35120c978d71b0252
Jason Harrison <120011133+j0hnny0rang3@users.noreply.github.com>
2026-06-08 21:43:20 +0100

    Update c2c.js

--- c2c.js
+++ c2c.js
@@ -276,7 +276,9 @@ function c2c_initStack(account) {
 }
 
 async function c2c_sbc_connect_sequence() {
-	await c2c_selectDevices();
+	if(c2c_phone.getOS() === 'windows'){
+		await c2c_selectDevices();
+	}
 	c2c_initStack({ user: caller, displayName: callerDn, password: '' });
 	
 }
